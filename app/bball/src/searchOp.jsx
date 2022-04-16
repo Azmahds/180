6a7936c1fb8d4c057fd511d9db006112752bb911
Azmahds <azmahds@gmail.com>
2022-04-16 15:11:16 -0700

    added data

--- app/bball/src/searchOp.jsx
+++ app/bball/src/searchOp.jsx
@@ -1,42 +1,42 @@
 import React, { Component } from "react";
 
 class SearchOp extends Component{
+    state = {
+        game_details: [],
+        games: [],
+        players: [],
+        ranking: [],
+        teams: [],
+    };
+
     render(){
         return(
             <React.Fragment>
-                {/* <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    {/* <link rel=”stylesheet” href=”https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity=”sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T” crossorigin=”anonymous”>
-                    <title>Document</title>
-                </head> */}
-                <body>
-                    <div class="container">
-                        <div class="col-md-12">
-                            <h1>Find Players</h1>
-                            <h2>Enter the name of a Player</h2>
-                            <form id="form">
-                                <input class="form-control" id="user-input" placeholder="Enter player..." />
-                                <button id="button" class="btn btn-secondary">Find NBA Players</button>
-                            </form>
-                            <table class="table" cellpadding="10">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Player</th>
-                                        <th scope="col">Team ID</th>
-                                        <th scope="col">Player ID</th> 
-                                        <th scope="col">Season</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                <div class="container">
+                    <div class="col-md-12">
+                        <h1>Find Players</h1>
+                        <h2>Enter the name of a Player</h2>
+                        <form id="form">
+                            <input class="form-control" id="user-input" placeholder="Enter player..." />
+                            <button id="button" class="btn btn-secondary">Find NBA Players</button>
+                        </form>
+                        <table class="table" cellpadding="10">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Player</th>
+                                    <th scope="col">Team ID</th>
+                                    <th scope="col">Player ID</th> 
+                                    <th scope="col">Season</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
-                </body>
+                </div>
             </React.Fragment>
         );
     }
