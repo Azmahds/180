4073524791ffae9b522dc1ecc2d474d643eacdf1
Azmahds <azmahds@gmail.com>
2022-04-17 21:57:29 -0700

    working filter

--- src/public/csv.js
+++ src/public/csv.js
@@ -1,9 +1,73 @@
+
 window.addEventListener("load", (event) =>{
     console.log("Page loaded");
-    var temp = this.document.getElementById("test");
-    console.log("CONSOLE", temp);
-    temp.innerHTML = "good morning";
+    let data = getData();
+    createTable(data);
+    document.getElementById("buttonid").addEventListener("click", () => filterPlayer(data))
 })
 
+function getData(){
+    let data = [{"PLAYER_NAME":"Royce O'Neale","TEAM_ID":"1610612762","PLAYER_ID":"1626220","SEASON":"2019"},{"PLAYER_NAME":"Bojan Bogdanovic","TEAM_ID":"1610612762","PLAYER_ID":"202711","SEASON":"2019"},{"PLAYER_NAME":"Rudy Gobert","TEAM_ID":"1610612762","PLAYER_ID":"203497","SEASON":"2019"},{"PLAYER_NAME":"Donovan Mitchell","TEAM_ID":"1610612762","PLAYER_ID":"1628378","SEASON":"2019"},{"PLAYER_NAME":"Mike Conley","TEAM_ID":"1610612762","PLAYER_ID":"201144","SEASON":"2019"},{"PLAYER_NAME":"Joe Ingles","TEAM_ID":"1610612762","PLAYER_ID":"204060","SEASON":"2019"},{"PLAYER_NAME":"Ed Davis","TEAM_ID":"1610612762","PLAYER_ID":"202334","SEASON":"2019"},{"PLAYER_NAME":"Jeff Green","TEAM_ID":"1610612762","PLAYER_ID":"201145","SEASON":"2019"},{"PLAYER_NAME":"Dante Exum","TEAM_ID":"1610612762","PLAYER_ID":"203957","SEASON":"2019"},{"PLAYER_NAME":"Emmanuel Mudiay","TEAM_ID":"1610612762","PLAYER_ID":"1626144","SEASON":"2019"},{"PLAYER_NAME":"Georges Niang","TEAM_ID":"1610612762","PLAYER_ID":"1627777","SEASON":"2019"},{"PLAYER_NAME":"Tony Bradley","TEAM_ID":"1610612762","PLAYER_ID":"1628396","SEASON":"2019"}]
+    return data;
+}
 
+function createTable(data){
+    var table = document.getElementById("body");
 
+    for (let i = 0; i < data.length; i++) {
+        let player = [data[i].PLAYER_NAME, data[i].TEAM_ID, data[i].PLAYER_ID, data[i].SEASON];
+        var row = table.insertRow(-1);
+        for(let j = 0; j < player.length + 1; ++j){
+            var cell = row.insertCell(j);
+            if(j == 0){
+                cell.innerHTML = i + 1;
+            }
+            else{
+                cell.innerHTML = player[j-1];
+            }
+        }
+    }
+}
+
+function filterPlayer(data){
+    const input = document.getElementById("user-input").value;
+    var table = document.getElementById("body");
+    table.innerHTML = "";
+    
+    // console.log(typeof input, input.length);
+    if(input.length == 0){
+        createTable(data);
+        return;
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        let player = [data[i].PLAYER_NAME, data[i].TEAM_ID, data[i].PLAYER_ID, data[i].SEASON];
+        if(!contains(player[0].toLowerCase(),input.toLowerCase())){
+            continue;
+        }
+        var row = table.insertRow(-1);
+        for(let j = 0; j < player.length + 1; ++j){
+            var cell = row.insertCell(j);
+            if(j == 0){
+                cell.innerHTML = i + 1;
+            }
+            else{
+                cell.innerHTML = player[j-1];
+            }
+        }
+    }
+}
+
+function contains(tar, substr){
+    for(let i = 0; i < tar.length; ++i){
+        if(tar[i] == substr[0]){
+            for(let j = 0; j < substr.length; ++j){
+                if(tar[i+j] != substr[j]){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    return false
+}
--- src/server.js
+++ src/server.js
@@ -2,63 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const fs = require("fs");
-csv = fs.readFileSync("teams.csv")
-
-async function csvTOJson(){
-  
-  var array = csv.toString().split("\n");
-
-  
-  let result = [];
-
- 
-  let headers = array[0].split(",")
-
-
- 
-  for (let i = 1; i < array.length - 1; i++) {
-  let obj = {}
-
-  
-  let str = array[i]
-  let s = ''
-
-  
-  let flag = 0
-  for (let ch of str) {
-      if (ch === '"' && flag === 0) {
-      flag = 1
-      }
-      else if (ch === '"' && flag == 1) flag = 0
-      if (ch === ',' && flag === 0) ch = '|'
-      if (ch !== '"') s += ch
-    }
-
-    
-    let properties = s.split("|")
-
-  
-    for (let j in headers) {
-      obj[headers[j]] = properties[j]
-    }
-
-   
-    result.push(obj)
-
-  }
-
- 
-  let json = JSON.stringify(result);
-  fs.writeFileSync('output.json', json);
-
-}
-
-csvTOJson();
 
 // app.get("/", function (req, res) {
 //   res.sendFile(__dirname + "/index.html");
 // });
+
 
 app.set("view engine", "ejs");
 
@@ -71,3 +19,39 @@ app.get("/", function (req, res) {
 app.listen(3000, function () {
     console.log("Server is running on localhost3000");
 });
+
+const fs = require("fs")
+let csv = fs.readFileSync("./public/data/players.csv")
+
+async function csvTOJson(){
+  var array = csv.toString().split("\n");
+  let result = []; 
+  let headers = array[0].split(",")
+ 
+  for (let i = 1; i < array.length - 1; i++) {
+  let obj = {}
+  let str = array[i]
+  let s = ''
+  
+  let flag = 0
+  for (let ch of str) {
+      if (ch === '"' && flag === 0) {
+      flag = 1
+      }
+      else if (ch === '"' && flag == 1) flag = 0
+      if (ch === ',' && flag === 0) ch = '|'
+      if (ch !== '"') s += ch
+    }
+    
+    let properties = s.split("|")
+
+    for (let j in headers) {
+      obj[headers[j]] = properties[j]
+    }
+    result.push(obj)
+  }
+  let json = JSON.stringify(result);
+  fs.writeFileSync('output.json', json);
+}
+
+csvTOJson();
