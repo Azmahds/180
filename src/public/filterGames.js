f5cf1e37e738c28af54ffd521e58c54ffa8da664
Azmahds <azmahds@gmail.com>
2022-04-28 19:59:00 -0700

    added table to games and filter functionality

--- src/public/filterGames.js
+++ src/public/filterGames.js
@@ -1,6 +1,6 @@
-
 window.addEventListener("load", (event) =>{
     console.log("Page loaded");
+    populateOptions();
     createTable();
     document.getElementById("buttonid").addEventListener("click", () => filterGD())
 })
@@ -8,15 +8,36 @@ window.addEventListener("load", (event) =>{
 function getData(){
     let allD = document.getElementById("gamesData");
     let data = JSON.parse(allD.innerText)
+    // var this_js_script = $('script[src*=filterGames]'); //get data passed through from script
+    // var data = JSON.parse(this_js_script.attr('all_data'));
     return data;
+}
+
+function populateOptions(){
+    let options = [];
+    var data = getData();
+    data.forEach(element => {
+        if(options.indexOf(element.HOME_TEAM_ID) == -1){
+            console.log("push")
+            options.push(element.HOME_TEAM_ID);
+        }
+    });
+    let select = document.getElementById("user-input");
+
+    options.forEach(el => {
+        var option = document.createElement("option");
+        option.value = el;
+        option.text = el;
+        select.add(option);
+    });
 }
 
 function createTable(){
     var data = getData();
     var table = document.getElementById("body");
-
+    
     for (let i = 0; i < data.length; i++) {
-        let player = [data[i].PLAYER_NAME, data[i].TEAM_ID, data[i].PLAYER_ID, data[i].SEASON];
+        let player = [data[i].HOME_TEAM_ID, data[i].VISITOR_TEAM_ID, data[i].PTS_home, data[i].REB_home, data[i].AST_home , data[i].FG_PCT_home , data[i].FT_PCT_home , data[i].FG3_PCT_home , data[i].PTS_away , data[i].REB_away , data[i].AST_away , data[i].FG_PCT_away , data[i].FT_PCT_away , data[i].FG3_PCT_away];
         var row = table.insertRow(-1);
         for(let j = 0; j < player.length + 1; ++j){
             var cell = row.insertCell(j);
@@ -35,8 +56,7 @@ function filterGD(){
     let input = document.getElementById("user-input").value;
     var table = document.getElementById("body");
     var rows = table.children;
-    
-    console.log(typeof input, input.length, input);
+
     if(input.length == 0){
         for(let i = 0; i < data.length; ++i){
             if(rows[i].hidden){
@@ -48,9 +68,9 @@ function filterGD(){
     }
     var cnt = 0;
     for (let i = 0; i < data.length; i++) {
-        // let player = [data[i].PLAYER_NAME, data[i].TEAM_ID, data[i].PLAYER_ID, data[i].SEASON];
-        let team = data[i].TEAM_ID
-        if(team != input){
+        let home = data[i].HOME_TEAM_ID
+        let visitor = data[i].VISITOR_TEAM_ID
+        if(home != input && visitor != input){
             rows[i].hidden = true;
             continue;
         }
