2e3daddc392cbc3124d364e731b049133dc2a9e5
Azmahds <azmahds@gmail.com>
2022-05-05 18:22:40 -0700

    setup for images

--- src/public/filterConsistent.js
+++ src/public/filterConsistent.js
@@ -42,8 +42,9 @@ function createTable(){ //NUMBER, TEAM NAME, TEAM_ID, TOTAL WINS
             let player =  data[i];
             var row = table.insertRow(-1);
             var pidx = 0;
+            var cell;
             for(let j = 0; j < 4; ++j){
-                var cell = row.insertCell(j);
+                cell = row.insertCell(j);
                 if(j == 0){
                     cell.innerHTML = i + 1;
                 }
@@ -56,6 +57,15 @@ function createTable(){ //NUMBER, TEAM NAME, TEAM_ID, TOTAL WINS
                 }
                 cell.style = "text-align: center"
             }
+            if(data == all){
+                continue;
+            }
+            
+            cell = row.insertCell(-1);
+            var img = document.createElement("img");
+            img = new Image(50,50);
+            img.src = getImage(player[0]);
+            cell.appendChild(img)
         }
     }
 }
@@ -112,6 +122,106 @@ function consistentTeamsTable(){
 }
 
 function match(id) {
+    var teamID = id;
+    var teamName = "N/A";
+
+    if(teamID == 1610612738) {
+        teamName = "BOS";
+
+    }
+    else if(teamID == 1610612748) {
+        teamName = "MIA";
+    }
+    else if(teamID == 1610612740) {
+        teamName = "NOP";
+    }
+    else if(teamID == 1610612745) {
+        teamName = "HOU";
+    }
+    else if(teamID == 1610612737) {
+        teamName = "ATL";
+    }
+    else if(teamID == 1610612751) {
+        teamName = "BKN";
+    }
+    else if(teamID == 1610612765) {
+        teamName = "DET";
+    }
+    else if(teamID == 1610612744) {
+        teamName = "GSW";
+    }
+    else if(teamID == 1610612747) {
+        teamName = "LAL";
+    }
+    else if(teamID == 1610612750) {
+        teamName = "MIN";
+    }
+    else if(teamID == 1610612753) {
+        teamName = "ORL";
+    }
+    else if(teamID == 1610612755) {
+        teamName = "PHI";
+    }
+    else if(teamID == 1610612762) {
+        teamName = "UTA";
+    }
+    else if(teamID == 1610612746) {
+        teamName = "LAC";
+    }
+    else if(teamID == 1610612741) {
+        teamName = "CHI";
+    }
+    else if(teamID == 1610612743) {
+        teamName = "DEN";
+    }
+    else if(teamID == 1610612756) {
+        teamName = "PHX";
+    }
+    else if(teamID == 1610612757) {
+        teamName = "POR";
+    }
+    else if(teamID == 1610612758) {
+        teamName = "SAC";
+    }
+    else if(teamID == 1610612760) {
+        teamName = "OKC";
+    }
+    else if(teamID == 1610612764) {
+        teamName = "WAS";
+    }
+    else if(teamID == 1610612766) {
+        teamName = "CHA";
+    }
+    else if(teamID == 1610612742) {
+        teamName = "DAL";
+
+    }
+    else if(teamID == 1610612749) {
+        teamName = "MIL";
+    }
+    else if(teamID == 1610612752) {
+        teamName = "NYK";
+    }
+    else if(teamID == 1610612754) {
+        teamName = "IND";
+    }
+    else if(teamID == 1610612759) {
+        teamName = "SAS";
+    }
+    else if(teamID == 1610612761) {
+        teamName = "TOR";
+    }
+    else if(teamID == 1610612763) {
+        teamName = "MEM";
+    }
+    else if(teamID == 1610612739) {
+        teamName = "CLE";
+    }
+
+    return teamName;
+}
+
+function getImage(id){
     var teamID = id;
     var teamName = "N/A";
 
